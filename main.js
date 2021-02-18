55245bb8c9cc69c3b8173d997af9de224225af42
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-18 02:19:28 -0600

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const errorModal = document.querySelector('#modal');
+errorModal.classList.add('hidden');
+let hearts = document.querySelectorAll('.like-glyph');
+for (const heart of hearts) {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then(() => {
+      if (heart.innerHTML != FULL_HEART) {
+        heart.innerHTML = FULL_HEART;
+        heart.classList.add('activated-heart');
+      } else {
+        heart.innerHTML = EMPTY_HEART;
+        heart.classList.remove;
+      }
+    })
+    .catch(error => {
+      errorModal.classList.remove('hidden');
+      window.setTimeout(() => {errorModal.classList.add('hidden')}, 2500);
+      errorModal.innerHTML = error;
+      console.log(error);
+    });
+  }
+  )}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
